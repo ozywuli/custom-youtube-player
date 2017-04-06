cc5743bed6fab6611e5504919781736ef1fc2a4e
ozywuli <ozywuli@gmail.com>
2017-04-05 18:55:14 -0700

    initialize yt

---
+++ script.js
@@ -0,0 +1,5 @@
+var tag = document.createElement('script');
+tag.id = 'youtube-iframe';
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
