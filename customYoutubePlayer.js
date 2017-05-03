a30d78dd1912996efcd8211805492c876699ab6e
ozywuli <ozy.wu.li@gmail.com>
2017-05-03 16:46:14 -0700

    remove old files

--- customYoutubePlayer.js
+++ customYoutubePlayer.js
@@ -1,5 +1,6 @@
 ;(function() {
 
+    // ADD YOUTUBE IFRAME API SCRIPT
     var tag = document.createElement('script');
     tag.id = 'youtube-iframe';
     tag.src = 'https://www.youtube.com/iframe_api';
@@ -11,166 +12,141 @@
     var $youtubeIframe = $('#youtube-player-iframe');
     var $adaptiveEl;
 
+    // IIFE
     CustomYouTubePlayer = (function() {
 
+        /*------------------------------------*\
+          CUSTOM YOUTUBE IFRAME API
+        \*------------------------------------*/
         function CustomYouTubePlayer(userOptions) {
+            var self = this;
 
-            console.log(userOptions.native);
+            // YOUTUBE IFRAME API PLAYER
+            this.Player = undefined;
 
-            var defaultNativeOptions = {
-                events: {
-                    'onReady': onPlayerReady,
-                    'onStateChange': onPlayerStateChange
+
+            /*------------------------------------*\
+              CUSTOM METHODS
+            \*------------------------------------*/
+            self.customMethods = {
+                onPlayerReady: function() {
+                    // TURN ON ADAPTIVE VIDEO IF USER HAS SELECTED IT
+                    if (combinedExtendOptions.adaptiveVid) {
+                        self.customMethods.turnOnAdaptiveVid();
+                    }
+                },
+
+                onPlayerStateChange: function() {
+                    var playerState = self.Player.getPlayerState()
+                    
+                    // WHEN THE VIDEO HAS ENDED
+                    if (playerState === 0) {
+                        combinedExtendOptions.onVidEnd();
+                    }
+                },
+
+                // OPTION TO TURN OFF ADAPTIVE VIDEO DYNAMICALLY
+                turnOffAdaptiveVid: function() {
+                    $adaptiveEl.off('resize.ytModal');
+                },
+
+                // ADPATIVE VIDEO STUFF
+                turnOnAdaptiveVid: function() {
+                    $youtubeIframe = $('#youtube-player-iframe');
+                    $adaptiveEl = $(combinedExtendOptions.adaptiveVidParent);
+
+                    // LOOP THROUGH IFRAME IFRAME AND SET THE ASPECT RATIO BASED ON THE IFRAME'S DEFAULT WIDTH AND HEIGHT
+                    $youtubeIframe.each(function() {
+                        $(this)
+                            .attr('data-aspectratio', this.height / this.width)
+                            .attr('data-aspectratio-h', this.width / this.height)
+                            .removeAttr('height width')
+                    });
+
+                    // RESIZE IFRAME FUNCTION
+                    function resizeIframe() {
+
+                        // NEW WIDTH BASED ON IFRAME'S ORIGINAL WIDTH AND THE DESIRED DIMENSIONS
+                        var newWidth = $adaptiveEl.width() * combinedExtendOptions.adaptiveVidDimensions;
+                        // NEW HEIGHT BASED ON IFRAME'S ORIGINAL HEIGHT AND THE DESIRED DIMENSIONS
+                        var newHeight = $adaptiveEl.height() * combinedExtendOptions.adaptiveVidDimensions;
+
+                        // RESIZE BASE ON WIDTH
+                        if ( ($adaptiveEl.height() / $adaptiveEl.width()) > $youtubeIframe.attr('data-aspectratio') ) {
+                            $youtubeIframe.each(function() {
+                                var $thisEl = $(this);
+                                $thisEl
+                                    .width(newWidth)
+                                    .height(newWidth * $thisEl.attr('data-aspectratio'))
+                            });
+
+                        // RESIZE BASE ON HEIGHT
+                        } else {
+                            $youtubeIframe.each(function() {
+                                var $thisEl = $(this);
+                                $thisEl
+                                    .width(newHeight * $thisEl.attr('data-aspectratio-h'))
+                                    .height(newHeight)
+                            });
+                        }
+                    }
+
+                    // CALL `resizeIframe` TO GET THE BALL ROLLING
+                    resizeIframe();
+
+                    // ONCE IFRAME HAS BEEN RESIZED, REVEAL THE IFRAME
+                    $youtubeIframe.css('opacity', 1);
+
+                    // RESIZE IFRAME SHOULD WORK WHEN THE WINDOW RESIZES
+                    $adaptiveEl.on('resize.ytModal', function() {
+                        resizeIframe();
+                    });
                 }
             }
 
-            var daNativeOptions = $.extend(defaultNativeOptions, userOptions.native);
+
+            /*------------------------------------*\
+              NATIVE YOUTUBE IFRAME API
+            \*------------------------------------*/
+            var defaultNativeOptions = {
+                events: {
+                    'onReady': self.customMethods.onPlayerReady,
+                    'onStateChange': self.customMethods.onPlayerStateChange
+                }
+            }
+
+            var combinedNativeOptions = $.extend(defaultNativeOptions, userOptions.native)
+
+            window.onYouTubeIframeAPIReady = function() {
+                self.Player = new YT.Player('youtube-player-iframe', combinedNativeOptions);
+            };
 
 
-            var defaultCustomOptions = {
+            /*------------------------------------*\
+              EXTENDED YOUTUBE IFRAME API
+            \*------------------------------------*/
+            var defaultExtendOptions = {
                 adaptiveVid: true,
                 adaptiveVidParent: window,
                 adaptiveVidDimensions: 0.9,
                 onVidEnd: null
             }
 
-            // https://api.jquery.com/jquery.extend/
-            var daCustomOptions = $.extend(defaultCustomOptions, userOptions.custom);
-
-            window.onYouTubeIframeAPIReady = function() {
-
-                Player = new YT.Player('youtube-player-iframe', defaultNativeOptions);
-
-            }
+            var combinedExtendOptions = $.extend(defaultExtendOptions, userOptions.extend);
 
             // HIDE THE YOUTUBE IFRAME IF ADAPTIVE VIDEO IS TURNED ON SO THAT USERS DON'T SEE THE IFRAME BEING RESIZED
-            if (daCustomOptions.adaptiveVid) {
+            if (combinedExtendOptions.adaptiveVid) {
                 $youtubeIframe.css('opacity', 0);
             }
 
-            // AFTER YOUTUBE API HAS BEEN LODED
-            function onPlayerReady() {
-                // TURN ON ADAPTIVE VIDEO IF USER HAS SELECTED IT
-                if (daCustomOptions.adaptiveVid) {
-                    turnOnAdaptiveVid();
-                }
-            }
+        } // END `CustomYouTubePlayer()`
 
-            // ON PLAYER STATE CHANGE
-            function onPlayerStateChange() {
-                var playerState = Player.getPlayerState()
-                
-
-                // WHEN THE VIDEO HAS ENDED
-                if (playerState === 0) {
-                    // https://github.com/nolimits4web/Swiper/blob/master/src/js/core.js
-                    // http://stackoverflow.com/questions/32246262/what-is-emit-javascript-function
-                    // http://stackoverflow.com/questions/1553342/custom-event-in-jquery-that-isnt-bound-to-a-dom-element#comment1417316_1556914
-                    // http://stackoverflow.com/questions/4942639/add-event-to-jquery-plugin
-                    // TRIGGER THE `onVidEnd` CALLBACK
-                    daCustomOptions.onVidEnd();
-                }
-
-            }
-
-            // ADPATIVE VIDEO STUFF
-            function turnOnAdaptiveVid() {
-
-                $youtubeIframe = $('#youtube-player-iframe');
-                $adaptiveEl = $(daCustomOptions.adaptiveVidParent);
-
-                // LOOP THROUGH IFRAME IFRAME AND SET THE ASPECT RATIO BASED ON THE IFRAME'S DEFAULT WIDTH AND HEIGHT
-                $youtubeIframe.each(function() {
-                    $(this)
-                        .attr('data-aspectratio', this.height / this.width)
-                        .attr('data-aspectratio-h', this.width / this.height)
-                        .removeAttr('height width')
-                });
-
-                // RESIZE IFRAME FUNCTION
-                function resizeIframe() {
-
-                    // NEW WIDTH BASED ON IFRAME'S ORIGINAL WIDTH AND THE DESIRED DIMENSIONS
-                    var newWidth = $adaptiveEl.width() * daCustomOptions.adaptiveVidDimensions;
-                    // NEW HEIGHT BASED ON IFRAME'S ORIGINAL HEIGHT AND THE DESIRED DIMENSIONS
-                    var newHeight = $adaptiveEl.height() * daCustomOptions.adaptiveVidDimensions;
-
-                    // RESIZE BASE ON WIDTH
-                    if ( ($adaptiveEl.height() / $adaptiveEl.width()) > $youtubeIframe.attr('data-aspectratio') ) {
-                        $youtubeIframe.each(function() {
-                            var $thisEl = $(this);
-                            $thisEl
-                                .width(newWidth)
-                                .height(newWidth * $thisEl.attr('data-aspectratio'))
-                        });
-
-                    // RESIZE BASE ON HEIGHT
-                    } else {
-                        $youtubeIframe.each(function() {
-                            var $thisEl = $(this);
-                            $thisEl
-                                .width(newHeight * $thisEl.attr('data-aspectratio-h'))
-                                .height(newHeight)
-                        });
-                    }
-                }
-
-                // CALL `resizeIframe` TO GET THE BALL ROLLING
-                resizeIframe();
-
-                // ONCE IFRAME HAS BEEN RESIZED, REVEAL THE IFRAME
-                $youtubeIframe.css('opacity', 1);
-
-                // RESIZE IFRAME SHOULD WORK WHEN THE WINDOW RESIZES
-                $adaptiveEl.on('resize.ytModal', function() {
-                    resizeIframe();
-                });
-
-            }
-
-            // OPTION TO TURN OFF ADAPTIVE VIDEO DYNAMICALLY
-            function turnOffAdaptiveVid() {
-                $adaptiveEl.off('resize.ytModal');
-            }
-
-        }
-
-
-        /*------------------------------------*\
-          PLAYBACK CONTROLS
-        \*------------------------------------*/
-        CustomYouTubePlayer.prototype.playVideo = function() {
-            Player.playVideo();
-        }
-
-        CustomYouTubePlayer.prototype.stopVideo = function() {
-            Player.stopVideo();
-        }
-
-        CustomYouTubePlayer.prototype.pauseVideo = function() {
-            Player.pauseVideo();
-        }
-
-
-
-        // RETURN `CustomYouTubePlayer` SO THAT IT CAN BE INSTANTIATED
+        // RETURN
         return CustomYouTubePlayer;
+        
+    })(); // END `CustomYouTubePlayer` IIFE
 
-    })();
-
-
-    /*------------------------------------*\
-      EXPORT OPTIONS
-    \*------------------------------------*/
-    if (typeof define === 'function' && define.amd) {
-        define([], function() {
-            return CustomYouTubePlayer;
-        });
-    } else if (typeof exports !== "undefined" && exports !== null) {
-        module.exports = CustomYouTubePlayer;
-    } else {
-        window.CustomYouTubePlayer = CustomYouTubePlayer;
-    }
-
+    // SET GLOBAL
+    window.CustomYouTubePlayer = CustomYouTubePlayer;
 
 }).call(this);
