0d4877a15ef3fa7b45007d4a4710aee5f1062197
ozywuli <ozywuli@gmail.com>
2017-05-03 16:22:34 -0700

    add in custom methods

--- ExtendYoutubePlayer.js
+++ ExtendYoutubePlayer.js
@@ -1,5 +1,6 @@
 ;(function() {
 
+    // ADD YOUTUBE IFRAME API SCRIPT
     var tag = document.createElement('script');
     tag.id = 'youtube-iframe';
     tag.src = 'https://www.youtube.com/iframe_api';
@@ -12,62 +13,143 @@
     var $adaptiveEl;
 
 
-    
+    // IIFE
+    CustomYouTubePlayer = (function() {
+
+        /*------------------------------------*\
+          CUSTOM YOUTUBE IFRAME API
+        \*------------------------------------*/
+        function CustomYouTubePlayer(userOptions) {
+            var self = this;
+
+            // YOUTUBE IFRAME API PLAYER
+            this.Player = undefined;
+
+
+            /*------------------------------------*\
+              NATIVE YOUTUBE IFRAME API
+            \*------------------------------------*/
+            var defaultNativeOptions = {
+                events: {
+                    'onReady': onPlayerReady,
+                    'onStateChange': onPlayerStateChange
+                }
+            }
+
+            var combinedNativeOptions = $.extend(defaultNativeOptions, userOptions.native)
+
+            window.onYouTubeIframeAPIReady = function() {
+                self.Player = new YT.Player('youtube-player-iframe', combinedNativeOptions);
+            };
+
+
+            /*------------------------------------*\
+              EXTENDED YOUTUBE IFRAME API
+            \*------------------------------------*/
+            var defaultExtendOptions = {
+                adaptiveVid: true,
+                adaptiveVidParent: window,
+                adaptiveVidDimensions: 0.9,
+                onVidEnd: null
+            }
+
+            var combinedExtendOptions = $.extend(defaultExtendOptions, userOptions.extend);
+
+
+            /*------------------------------------*\
+              CUSTOM METHODS
+            \*------------------------------------*/
+            // HIDE THE YOUTUBE IFRAME IF ADAPTIVE VIDEO IS TURNED ON SO THAT USERS DON'T SEE THE IFRAME BEING RESIZED
+            if (combinedExtendOptions.adaptiveVid) {
+                $youtubeIframe.css('opacity', 0);
+            }
+            
+            function onPlayerReady() {
+                // TURN ON ADAPTIVE VIDEO IF USER HAS SELECTED IT
+                if (combinedExtendOptions.adaptiveVid) {
+                    turnOnAdaptiveVid();
+                }
+            }
+
+            function onPlayerStateChange() {
+                var playerState = self.Player.getPlayerState()
+                
+                // WHEN THE VIDEO HAS ENDED
+                if (playerState === 0) {
+                    combinedExtendOptions.onVidEnd();
+                }
+            }
+
+            // ADPATIVE VIDEO STUFF
+            function turnOnAdaptiveVid() {
+
+                $youtubeIframe = $('#youtube-player-iframe');
+                $adaptiveEl = $(combinedExtendOptions.adaptiveVidParent);
+
+                // LOOP THROUGH IFRAME IFRAME AND SET THE ASPECT RATIO BASED ON THE IFRAME'S DEFAULT WIDTH AND HEIGHT
+                $youtubeIframe.each(function() {
+                    $(this)
+                        .attr('data-aspectratio', this.height / this.width)
+                        .attr('data-aspectratio-h', this.width / this.height)
+                        .removeAttr('height width')
+                });
+
+                // RESIZE IFRAME FUNCTION
+                function resizeIframe() {
+
+                    // NEW WIDTH BASED ON IFRAME'S ORIGINAL WIDTH AND THE DESIRED DIMENSIONS
+                    var newWidth = $adaptiveEl.width() * combinedExtendOptions.adaptiveVidDimensions;
+                    // NEW HEIGHT BASED ON IFRAME'S ORIGINAL HEIGHT AND THE DESIRED DIMENSIONS
+                    var newHeight = $adaptiveEl.height() * combinedExtendOptions.adaptiveVidDimensions;
+
+                    // RESIZE BASE ON WIDTH
+                    if ( ($adaptiveEl.height() / $adaptiveEl.width()) > $youtubeIframe.attr('data-aspectratio') ) {
+                        $youtubeIframe.each(function() {
+                            var $thisEl = $(this);
+                            $thisEl
+                                .width(newWidth)
+                                .height(newWidth * $thisEl.attr('data-aspectratio'))
+                        });
+
+                    // RESIZE BASE ON HEIGHT
+                    } else {
+                        $youtubeIframe.each(function() {
+                            var $thisEl = $(this);
+                            $thisEl
+                                .width(newHeight * $thisEl.attr('data-aspectratio-h'))
+                                .height(newHeight)
+                        });
+                    }
+                }
+
+                // CALL `resizeIframe` TO GET THE BALL ROLLING
+                resizeIframe();
+
+                // ONCE IFRAME HAS BEEN RESIZED, REVEAL THE IFRAME
+                $youtubeIframe.css('opacity', 1);
+
+                // RESIZE IFRAME SHOULD WORK WHEN THE WINDOW RESIZES
+                $adaptiveEl.on('resize.ytModal', function() {
+                    resizeIframe();
+                });
+
+            }
+
+            // OPTION TO TURN OFF ADAPTIVE VIDEO DYNAMICALLY
+            function turnOffAdaptiveVid() {
+                $adaptiveEl.off('resize.ytModal');
+            }
 
 
 
-    window.onYouTubeIframeAPIReady = function() {
-
-        console.log(YT);
-
-        Player = new YT.Player();
-
-        console.log(Player);
-
-        Player.test = function() {
-            console.log('test');
         }
 
-        Player.test();
+        // RETURN
+        return CustomYouTubePlayer;
+        
+    })();
 
-        // ExtendPlayer = new YT.Player('youtube-player-iframe', {
-
-        // });
-
-        // console.log(ExtendPlayer);
-
-        ExtendPlayer = new Player();
-
-        console.log(ExtendPlayer);
-
-        // ExtendPlayer.test();
-
-    }
-
-
-    // var testObj = function() {
-    //     this.testProp = 1;
-    // }
-
-    // var Test = new testObj();
-
-    // console.log(Test);
-
-
-
-
-    /*------------------------------------*\
-      EXPORT OPTIONS
-    \*------------------------------------*/
-    // if (typeof define === 'function' && define.amd) {
-    //     define([], function() {
-    //         return CustomYouTubePlayer;
-    //     });
-    // } else if (typeof exports !== "undefined" && exports !== null) {
-    //     module.exports = CustomYouTubePlayer;
-    // } else {
-    //     window.CustomYouTubePlayer = CustomYouTubePlayer;
-    // }
-
+    // SET GLOBAL
+    window.CustomYouTubePlayer = CustomYouTubePlayer;
 
 }).call(this);
