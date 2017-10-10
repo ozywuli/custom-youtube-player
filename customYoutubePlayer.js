ccc1246ce32b89d83153fc6ee1303da1917ce977
ozywuli <ozy.wu.li@gmail.com>
2017-10-09 17:40:42 -0700

    update bower

--- customYoutubePlayer.js
+++ customYoutubePlayer.js
@@ -150,16 +150,14 @@
     /*------------------------------------*\
       EXPORT OPTIONS
     \*------------------------------------*/
-    // if (typeof define === 'function' && define.amd) {
-    //     define([], function() {
-    //         return CustomYouTubePlayer;
-    //     });
-    // } else if (typeof exports !== "undefined" && exports !== null) {
-    //     module.exports = CustomYouTubePlayer;
-    // } else {
-    //     window.CustomYouTubePlayer = CustomYouTubePlayer;
-    // }
-
-    window.CustomYouTubePlayer = CustomYouTubePlayer;
+    if (typeof define === 'function' && define.amd) {
+        define([], function() {
+            return CustomYouTubePlayer;
+        });
+    } else if (typeof exports !== "undefined" && exports !== null) {
+        module.exports = CustomYouTubePlayer;
+    } else {
+        window.CustomYouTubePlayer = CustomYouTubePlayer;
+    }
 
 }).call(this);
